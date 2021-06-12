8c41c0c5b0ec368735a20740bb23511cdcb39ec6
Denis O <denis@universal-development.com>
2021-06-12 09:17:16 +0300

    Basic application

--- src/main/java/com/game/rps/PaperRockScissorsApplication.java
+++ src/main/java/com/game/rps/PaperRockScissorsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RockPaperScissorsApplication {
+public class PaperRockScissorsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RockPaperScissorsApplication.class, args);
+		SpringApplication.run(PaperRockScissorsApplication.class, args);
 	}
 
 }

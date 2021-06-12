cd1dc587a2c4f3ba91ae015cec6b73ad67031e87
Denis O <denis@universal-development.com>
2021-06-12 12:00:50 +0300

    Game model update

---
+++ src/main/java/com/game/prs/model/GameEndResult.java
@@ -0,0 +1,8 @@
+package com.game.prs.model;
+
+/**
+ * Single game result
+ */
+public record GameEndResult(WinResult winResult, PlayerChoice player1, PlayerChoice player2) {
+
+}
--- src/main/java/com/game/prs/model/SessionState.java
+++ src/main/java/com/game/prs/model/SessionState.java
@@ -1,13 +1,17 @@
 package com.game.prs.model;
 
 /**
- * States in which game session can be
+ * States of game session
  */
 public enum SessionState {
 
+  INIT_SESSION,
+  NEW_SESSION,
   READ_GAME_COUNT,
   READ_PLAYER_INPUT,
   SHOW_GAME_RESULT,
-  SESSION_ENDED
+  SESSION_FINISHED,
+
+  INVALID_PLAYER_INPUT
 
 }

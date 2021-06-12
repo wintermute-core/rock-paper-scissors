77a43907c8d23a70f5e212ab9726d785194bb48a
Denis O <denis@universal-development.com>
2021-06-12 16:58:53 +0300

    Terminal mode fix

--- src/main/java/com/game/prs/service/ConsoleGameService.java
+++ src/main/java/com/game/prs/service/ConsoleGameService.java
@@ -4,13 +4,12 @@ import com.game.prs.game.HumanPlayer;
 import com.game.prs.game.Session;
 import com.game.prs.model.SessionState;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import javax.annotation.PostConstruct;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
@@ -32,39 +31,40 @@ public class ConsoleGameService extends AbstractGameServiceImpl {
     }
 
     taskScheduler.execute(terminalGame());
-
-
-
   }
 
   private Runnable terminalGame() {
-    return new Runnable() {
-      @SneakyThrows
-      @Override
-      public void run() {
-        log.info("""
-            Paper Rock Scissors game terminal mode
-            """);
-
-        while(true) {
-          BufferedReader inputReader = new BufferedReader(
-              new InputStreamReader(System.in));
-
-          System.out.println("Press Enter to start new game");
-          inputReader.readLine();
-          HumanPlayer humanPlayer = new HumanPlayer(
-              System.in, System.out
-          );
-
-          Session session = newSession(humanPlayer);
-          while (session.getSessionState() != SessionState.SESSION_FINISHED) {
-            session.update();
+    return () -> {
+      log.info("""
+          \n
+          Paper Rock Scissors game terminal mode \n
+          \n
+          """);
+      BufferedReader inputReader = new BufferedReader(
+          new InputStreamReader(System.in));
+      while(true) {
+        System.out.println(" *** Press Enter to start new game *** ");
+        try {
+          if (inputReader.readLine() == null) {
+            log.warn("Failed to access terminal");
+            break;
           }
-          session.clearSubscriptions();
-
+        } catch (IOException e) {
+          log.error("Failed to read line", e);
+          break;
         }
+        HumanPlayer humanPlayer = new HumanPlayer(
+            System.in, System.out
+        );
+
+        Session session = newSession(humanPlayer);
+        while (session.getSessionState() != SessionState.SESSION_FINISHED) {
+          session.update();
+        }
+        session.clearSubscriptions();
 
       }
+
     };
   }
 
